8e7bf939aeb31b6602dff943baffd3607dd74ae4
Tareq Judeh <tareq.joudeh@gmail.com>
2021-01-28 12:16:45 +0000

    Uploading file to GitHub.

--- main.py
+++ main.py
@@ -22,5 +22,28 @@ new_user.is_logged_in = True
 create_blog_post(new_user)
 
 
+# ================================================================================
 
-# Use the decorator 👇
+
+def decorator_function(function):
+    def wrapper_function():
+        function()
+    return wrapper_function
+
+
+def say_hello():
+    print("Hello")
+
+
+say_hello()
+
+
+def some_func(*args, **kwargs):
+    # *args and **kwargs:
+    age, height, weight, lucky_number = args
+    print(age, height, weight, lucky_number)
+    print(args[2])
+    print(f"My name is {kwargs['fname']} {kwargs['lname'] }")
+
+
+some_func(1, 2, 3, 4, fname="Tareq", lname="Joudeh")
