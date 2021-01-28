b192639629b390dcd3926104a3810c12ef005b05
Tareq Judeh <tareq.joudeh@gmail.com>
2021-01-28 12:14:33 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+# Create the logging_decorator() function 👇
+class User:
+  def __init__(self, name):
+    self.name = name
+    self.is_logged_in = False
+
+
+def is_authenticated_decorator(function):
+  def wrapper(*args):
+    if args[0].is_logged_in:
+      function(args[0])
+  return wrapper
+
+
+@is_authenticated_decorator
+def create_blog_post(user):
+  print(f"This is {user.name}'s blog post.") 
+
+new_user = User("Ali")
+new_user.is_logged_in = True
+
+create_blog_post(new_user)
+
+
+
+# Use the decorator 👇
